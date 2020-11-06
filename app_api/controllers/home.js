fa1a724e570e89489fc9605cec66225e6ea49eac
soheejjang0912 <soheejjang0912@gmail.com>
2020-11-06 14:14:47 +0900

    home app_api수정...

--- app_api/controllers/home.js
+++ app_api/controllers/home.js
@@ -1,49 +1,15 @@
 const mongoose = require('mongoose');
+const { post } = require('request');
 const Rest = mongoose.model('Restaurant');
- 
- 
-
-const restaurantsCategory = async (req, res) => {
-  const lng = parseFloat(req.query.lng);
-  const lat = parseFloat(req.query.lat);
-  const near = {
-    type: "Point",
-    coordinates: [lng, lat]
-  };
-  const geoOptions = {
-    distanceField: "distance.calculated",
-    key: 'coords',
-    spherical: true,
-    maxDistance: 20000,
-  };
-
-  if (!lng || !lat) {
-    return res.status(404).json({"message": "lng and lat query parameters are required"});
-  }
-
-  try {
-    const results = await Rest.aggregate([
-      {
-        $geoNear: {
-          near,
-          ...geoOptions
-        },
-      },
-      // {
-      //   $limit: 10
-      // }
-    ]);
-    const restaurants = results.map(result => {
-      return {
-        category: result.category, 
-        image: result.image, 
-      };
-    });
-    res.status(200).json(restaurants);
-  } catch (err) {
-    res.status(404).json(err);
-  }
-};
+  
+const restaurantsCategory = async (req, res, next) => {
+  try{
+    const posts = await Rest.find().exec();
+    res.status(200).json(posts);
+  }catch(e){
+    res.status(500).send(e);
+  }   
+} 
 
 module.exports = { 
   restaurantsCategory
