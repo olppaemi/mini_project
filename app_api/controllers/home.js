c464794b1e5244e90feb13a3cfd4853bd35a783d
soheejjang0912 <soheejjang0912@gmail.com>
2020-11-06 15:04:26 +0900

    home수정!

--- app_api/controllers/home.js
+++ app_api/controllers/home.js
@@ -5,12 +5,14 @@ const Rest = mongoose.model('Restaurant');
 const restaurantsCategory = async (req, res, next) => {
   try{
     const posts = await Rest.find().exec();
+    //posts = new Set(posts);
+    //const posts = await Rest.find().exec().distinct('category'); 
     res.status(200).json(posts);
   }catch(e){
     res.status(500).send(e);
   }   
 } 
-
+ 
 module.exports = { 
   restaurantsCategory
 };
